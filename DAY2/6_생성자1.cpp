88fc808830720b31f766a64577e5fb48c22d366f
ecourse <smkang@codenuri.co.kr>
2022-02-10 11:36:37 +0900

    Add files via upload

--- DAY2/6_생성자1.cpp
+++ DAY2/6_생성자1.cpp
@@ -6,10 +6,22 @@ class Point
 	int x;
 	int y;
 public:
-	void init()
+	// 생성자 : 1. 클래스 이름과 동일한 이름의 함수
+	//		    2. 반환 타입을 표기하지 않고, 값도 반환 할수 없다.
+	//          3. 객체를 생성하면 자동으로 생성자가 호출된다.
+	Point()
 	{
 		x = 0;
 		y = 0;
+		std::cout << "Point()" << std::endl;
+	}
+	// 생성자는 인자가 있어도 되고 없어도 됩니다.
+	// 2개 이상 만들수 있다는 의미(오버로딩 가능)
+	Point(int a, int b)
+	{
+		x = a;
+		y = b;
+		std::cout << "Point(int, int)" << std::endl;
 	}
 };
 int main()
@@ -19,5 +31,8 @@ int main()
 //	Point p1 = { 1, 1 }; 
 
 	Point pt;
-	pt.init();
+//	pt.init();
+
+	Point pt2(1, 2); // 인자가 2개인 생성자 호출
 }
+
