cb893249ae88362e6c65d743f574e90fd3e91e8c
ecourse <smkang@codenuri.co.kr>
2022-02-10 09:17:06 +0900

    Add files via upload

--- DAY2/1_std.cpp
+++ DAY2/1_std.cpp
@@ -1,12 +1,16 @@
 // 1_std.cpp
 #include <iostream>
-using namespace std;
+//using namespace std;
 
 int count = 0;
 
 int main()
 {
-	int n = count; 
+	int n = count; // C++ 표준에 "std::count()" 라는 함수가 있습니다.
+				   // 전역변수 count 와 표준 함수 count()를 헷갈려(ambigous)
+				   // 합니다.
+
+	std::cout << "aa" << std::endl;
 }
 
 // 컴파일 하면 에러가 발생합니다.
