0492856a223814e3a00c96926d9a2ec48ee31626
ecourse <smkang@codenuri.co.kr>
2022-02-10 14:38:51 +0900

    Add files via upload

--- DAY2/7_vector7.cpp
+++ DAY2/7_vector7.cpp
@@ -40,8 +40,14 @@ int main()
 
 	v.setAt(0, 3.4);
 	v.setAt(1, 2.1); 
-
 	v.resize(10); 
-
 	std::cout << v.getAt(1) << std::endl;  // 2.1
+
+	vector<int> v2(10);
+	v2.setAt(0, 3);
+	std::cout << v2.getAt(0) << std::endl;
 }
+// vector 라는 타입이 생겼으므로..
+// 크기가 변하는 배열이 필요할때 사용하면 됩니다.!!!
+// OOP : 타입을 만들어서 활용하자!!!!
+
