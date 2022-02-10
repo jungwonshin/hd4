6028f13a93325396a47f9b4cf811c8a5312c6120
ecourse <smkang@codenuri.co.kr>
2022-02-10 10:18:33 +0900

    Add files via upload

--- DAY2/4_oop1.cpp
+++ DAY2/4_oop1.cpp
@@ -1,4 +1,4 @@
-// 4_oop1 - 
+// 4_oop1 - 57 page (교재 예제와는 약간 다릅니다.)
 #include <iostream>
 
 // 사각형의 면적을 구하는 함수
@@ -7,7 +7,7 @@ void getRectArea(int left, int top, int right, int bottom)
 	return (right - left) * (bottom - top);
 }
 
-void drawRect()
+void drawRect(int left, int top, int right, int bottom)
 {
 	std::cout << "draw rect" << std::endl;
 }
@@ -15,5 +15,5 @@ void drawRect()
 int main()
 {
 	int n = getRectArea(1, 1, 10, 10);
-	drawRect();
+	drawRect(1, 1, 10, 10);
 }
