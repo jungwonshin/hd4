817827cd16b1e573e3ecbd5681c612e1846217a0
ecourse <smkang@codenuri.co.kr>
2022-02-10 10:38:07 +0900

    Add files via upload

--- DAY2/4_oop3.cpp
+++ DAY2/4_oop3.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
-// OOP 핵심 2.
+// OOP 핵심 2. 타입을 만들때
+//			  상태를 나타내는데이타와 상태를 가지고 연산을 수행하는 함수를 묶어서
+//		      타입을 만들자
+
+// C 구조체   : 함수를 포함할수 없다.
+// C++ 구조체 : 함수를 포함할수 있다.
+
 
 struct Rect
 {
@@ -13,11 +19,11 @@ struct Rect
 	// 멤버 함수 ( "메소드" 라고도 합니다.)
 	// 멤버 함수에서는 멤버 데이타를 바로 접근 가능합니다.
 	// => 그래서, 함수 인자로 데이타를 전달 받을 필요 없습니다.
-	void getRectArea()
+	void getArea()
 	{
 		return (right - left) * (bottom - top);
 	}
-	void drawRect()
+	void draw()
 	{
 		std::cout << "draw rect" << std::endl;
 	}
@@ -29,6 +35,6 @@ int main()
 //	int n = getRectArea(r);
 //	drawRect(r);
 
-	int n = r.getRectArea();
-	r.drawRect();
+	int n = r.getArea();
+	r.draw();
 }
