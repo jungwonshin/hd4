4c494667d56523352a6083e68398e6f3b8afc8d4
ecourse <smkang@codenuri.co.kr>
2022-02-10 14:13:49 +0900

    Add files via upload

--- DAY2/7_vector4.cpp
+++ DAY2/7_vector4.cpp
@@ -10,10 +10,25 @@
 
 class vector
 {
-
+	int* ptr;
+	int size;
+public:
+	vector(int sz)
+	{
+		size = sz;
+		ptr = new int[sz];
+	}
+	// 이 클래스는 생성자에서 자원할당(메모리할당)을 하고 있습니다.
+	// 이 경우 대부분 소멸자를 만들어서 메모리 해지를 해야 합니다.
+	~vector()
+	{
+		delete[] ptr;
+	}
 };
+
 int main()
 {
-	vector v(5); // 5개 짜리 동적 배열
-	v.resize(10);// 10개로 크기 변경.
+	vector v(5); // 5개 짜리 동적 배열. 이순간 메모리 그림을 그리세요
+				//					  생성자 코드가 실행되는 것까지 그리세요.
+//	v.resize(10);// 10개로 크기 변경.
 }
