36f6183d3ba486edd8f800e831a4b2f22b77e9cf
ecourse <smkang@codenuri.co.kr>
2022-02-10 09:22:28 +0900

    Add files via upload

--- DAY2/2_template.cpp
+++ DAY2/2_template.cpp
@@ -1,5 +1,5 @@
 // 2_template.cpp
-
+/*
 int square(int n)
 {
 	return n * n;
@@ -8,6 +8,15 @@ double square(double n)
 {
 	return n * n;
 }
+*/
+// 구현이 동일한 함수가 여러개 필요 하다면
+// "함수를 만들지 말고, 함수를 찍어낼때 사용할 "틀(template)"을 만들자
+template 
+int square(int n)
+{
+	return n * n;
+}
+
 int main()
 {
 	square(3);
