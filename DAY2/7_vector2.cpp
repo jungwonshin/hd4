c8ce14cb17fbe0ab3f4d6b70c99f3a3a062fd6ac
ecourse <smkang@codenuri.co.kr>
2022-02-10 13:11:00 +0900

    Add files via upload

---
+++ DAY2/7_vector2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+	// 2. 사용자에게 "5개의 정수"를 입력 받고 싶다.
+	int x[5];   
+
+	// 3. 사용자가 입력한 학생 수 만큼 점수를 다시 입력 받고 싶다.
+	int cnt = 0;
+	std::cout << "학생수는 >> ";
+	std::cin >> cnt;
+
+	int score[cnt]; // ???
+}
+
+
+
+
+
