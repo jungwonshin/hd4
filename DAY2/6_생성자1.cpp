a4d19e8164ebf5e8e483d5ae14abb5dd13b59a73
ecourse <smkang@codenuri.co.kr>
2022-02-10 11:31:55 +0900

    Add files via upload

--- DAY2/6_생성자1.cpp
+++ DAY2/6_생성자1.cpp
@@ -1,15 +1,23 @@
-// 5_생성자1 - 78 page
+// 6_생성자1 - 78 page
 #include <iostream>
 
 class Point
 {
-public:
 	int x;
 	int y;
+public:
+	void init()
+	{
+		x = 0;
+		y = 0;
+	}
 };
-
 int main()
 {
 	// 모든 멤버가 public 에 있다면 아래처럼 초기화 가능합니다(C언어 구조체 문법)
-	Point p1 = { 1, 1 };
+	// private 에 있다면 아래 코드는 에러 입니다.
+//	Point p1 = { 1, 1 }; 
+
+	Point pt;
+	pt.init();
 }
