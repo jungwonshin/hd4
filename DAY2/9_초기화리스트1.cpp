b16ebd54ce0900d2f034c55ba0b3be56f593f4c0
ecourse <smkang@codenuri.co.kr>
2022-02-10 15:46:16 +0900

    Add files via upload

--- DAY2/9_초기화리스트1.cpp
+++ DAY2/9_초기화리스트1.cpp
@@ -7,8 +7,11 @@ class Person
 	std::string name;
 	int age;
 public:
-	Person(const std::string& s, int a)
+	// 멤버 초기화 리스트(member initializer list) : 생성자 () 뒤에 :을 적고 멤버를 
+	//												초기화 하는것
+	Person(const std::string& s, int a) : name(s), age(a) // <== 이 코드가 좋습니다.
 	{
+		// 아래 처럼 초기화하는 것은 좋지 않습니다.
 		name = s;
 		age  = a; 
 	}
