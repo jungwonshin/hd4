9b65d314981c3a901c51508b01828650862f58a8
ecourse <smkang@codenuri.co.kr>
2022-02-11 16:10:53 +0900

    Add files via upload

--- DAY3/9_상속과생성자5.cpp
+++ DAY3/9_상속과생성자5.cpp
@@ -18,16 +18,26 @@ public:
 //    외부 또는 파생 클래스에서도 내부 멤버 데이타를 알 필요 없습니다.
 //    단지, Person 객체를 만들려면 "이름 과 나이"가 필요하다 는 것만 알면 됩니다.
 
+// 3. Student 설계자도 결국 Person 의 사용자 입니다.
+//    "기존에 있던 타입으로 부터 확장해서 설계하므로"!!
+
 class Student : public Person
 {
 	int id;
 public:
-	Student(int n) : id(n) {}
+	// 아래 생성자는 에러 입니다. 컴파일러가 생성한 코드(주석)을 참고 하세요
+//	Student(int n) : id(n) {} // Student(int n) : Person(), id(n) {} 이므로 error
+
+	// 모든 사람은 이름과 나이가 필요 하므로 아래 처럼 기반 클래스 생성자를 호출해야 합니다.
+	Student(const std::string& s, int a, int n) : Person(s, a),  id(n) {}
 };
+
+
 int main()
 {
 //	Person p1;  // error
 	Person p2("kim", 20);
 
-	Student s1(99);
+//	Student s1(99);
+	Student s2("kim", 20, 99);
 }
