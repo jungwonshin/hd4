4b8e641a48a323b3c66174f53dc924a3dab4d5ac
ecourse <smkang@codenuri.co.kr>
2022-02-11 14:05:46 +0900

    Add files via upload

--- DAY3/6_상수멤버함수1.cpp
+++ DAY3/6_상수멤버함수1.cpp
@@ -5,7 +5,6 @@ class Point
 {
 public:
 	int x, y;
-
 	Point(int a, int b) : x(a), y(b) {}
 
 	void set(int a, int b) 
@@ -13,15 +12,27 @@ public:
 		x = a;			   
 		y = b;			   
 	}
-	void print()
+
+	// 상수 멤버 함수(const member function)
+	// 멤버 함수 () 뒤에 const를 붙이는 문법
+	// 1. 해당 멤버 함수에서는 멤버데이타를 수정할수 없다.
+	// 2. 상수객체라도 상수 멤버 함수는 호출할수 있다.
+	void print() const 
 	{
 		std::cout << x << ", " << y << std::endl;
 	}
 };
 int main()
 {
-	Point p(1, 2);
-	p.x = 10;
-	p.set(10, 20);
-	p.print();
+	const Point p(1, 2);
+
+	// 아래 3줄 생각해 보세요
+	p.x = 10;     // error. public 에 있지만 상수 객체이므로
+	p.set(10, 20);// error. 상수객체 이므로 x, y 변경할수 없다!!
+	p.print();    // error
+				// 컴파일러는 멤버 함수의 선언만 보게 됩니다.
+				// 그래서, 멤버함수에서 "데이타를 수정하는지 알수 없습니다."
+				// 따라서, 상수객체일때는 멤버 함수를 호출할수 없습니다.
 }
+
+
