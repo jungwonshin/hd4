3847655c7c35670b894175f054de38ca7c91dfdd
ecourse <smkang@codenuri.co.kr>
2022-02-11 15:27:02 +0900

    Add files via upload

--- DAY3/9_상속과생성자1.cpp
+++ DAY3/9_상속과생성자1.cpp
@@ -1,6 +1,12 @@
 // 9_상속과생성자1 - 중요!!
 
 #include <iostream>
+// Derived 객체를 생성하면
+// 1. Base 생성자가 먼저 호출되고, Derived 생성자 호출
+// 2. Base 생성자는 항상 인자가 없는 디폴트 버전 호출
+// 3. 소멸자는 반대로!!
+
+// 그런데, 정확한 원리를 알아야 합니다.
 
 class Base
 {
@@ -10,7 +16,6 @@ public:
 	Base(int a) { std::cout << "Base(int)" << std::endl; }
 	~Base()     { std::cout << "~Base()"   << std::endl; }
 };
-
 class Derived : public Base
 {
 	int data2;
@@ -19,8 +24,8 @@ public:
 	Derived(int a) { std::cout << "Derived(int)" << std::endl; }
 	~Derived()     { std::cout << "~Derived()" << std::endl; }
 };
-
 int main()
 {
-	Derived d1; // 실행결과 예측해 보세요.
+//	Derived d1; // 실행결과 예측해 보세요.
+	Derived d2(5);
 }
