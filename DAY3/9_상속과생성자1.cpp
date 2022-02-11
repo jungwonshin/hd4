706724ed92c65f486b091b6bb58d505d27556352
ecourse <smkang@codenuri.co.kr>
2022-02-11 15:22:29 +0900

    Add files via upload

---
+++ DAY3/9_상속과생성자1.cpp
@@ -0,0 +1,25 @@
+// 9_상속과생성자1 - 중요!!
+
+#include <iostream>
+
+class Base
+{
+	int data;
+public:
+	Base()      { std::cout << "Base()"    << std::endl; }
+	Base(int a) { std::cout << "Base(int)" << std::endl; }
+	~Base()     { std::cout << "~Base()"   << std::endl; }
+};
+
+class Derived : public Base
+{
+public:
+	Derived()      { std::cout << "Derived()" << std::endl; }
+	Derived(int a) { std::cout << "Derived(int)" << std::endl; }
+	~Derived()     { std::cout << "~Derived()" << std::endl; }
+};
+
+int main()
+{
+	Derived d1;
+}
